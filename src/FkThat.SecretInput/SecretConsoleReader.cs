50e8330b5274174847d6b1e906a391a2efd1ea41
fkthat <fkthat@yandex.ru>
2023-10-24 11:07:02 +0300

    Main API

---
+++ src/FkThat.SecretInput/SecretConsoleReader.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using FkThat.Console;
+
+namespace FkThat.SecretInput;
+
+/// <inheritdoc/>
+public class SecretConsoleReader : ISecretConsoleReader
+{
+    private readonly IConsoleText _consoleText;
+    private readonly IConsoleKeyboard _consoleKeyboard;
+
+    /// <summary>
+    /// Initialize a new instance of the <see cref="SecretConsoleReader"/> class.
+    /// </summary>
+    /// <param name="consoleText">Console text I/O abstraction.</param>
+    /// <param name="consoleKeyboard">Console keyboard abstraction.</param>
+    /// <exception cref="ArgumentNullException">
+    /// The <paramref name="consoleText"/> or <paramref name="consoleKeyboard"/> is <see
+    /// langword="null"/>.
+    /// </exception>
+    public SecretConsoleReader(IConsoleText consoleText, IConsoleKeyboard consoleKeyboard)
+    {
+        _consoleText = consoleText ?? throw new ArgumentNullException(nameof(consoleText));
+        _consoleKeyboard = consoleKeyboard ?? throw new ArgumentNullException(nameof(consoleKeyboard));
+    }
+
+    /// <inheritdoc/>
+    [ExcludeFromCodeCoverage]
+    public string ReadLine(char maskChar = '*')
+    {
+        throw new NotImplementedException();
+    }
+}
