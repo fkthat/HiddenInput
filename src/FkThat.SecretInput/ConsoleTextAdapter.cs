3fda23bf8a4f7411cbfce84cd9f9cd73d37d876a
fkthat <fkthat@yandex.ru>
2023-11-07 00:08:51 +0300

    Text adapter implementation

---
+++ src/FkThat.SecretInput/ConsoleTextAdapter.cs
@@ -0,0 +1,41 @@
+﻿using FkThat.Console;
+
+namespace FkThat.SecretInput
+{
+    internal sealed class ConsoleTextAdapter : IKeyboardCommandHandler
+    {
+        private readonly IConsoleText _consoleText;
+        private readonly char _maskChar;
+
+        private int _count;
+
+        public ConsoleTextAdapter(IConsoleText consoleText, char maskChar)
+        {
+            _consoleText = consoleText;
+            _maskChar = maskChar;
+        }
+
+        public void Handle(CharKeyboardCommand command)
+        {
+            if (_maskChar != '\0')
+            {
+                _consoleText.Out.Write(_maskChar);
+                _count++;
+            }
+        }
+
+        public void Handle(BackspaceKeyboardCommand command)
+        {
+            if (_count > 0)
+            {
+                _consoleText.Out.Write("\b \b");
+                _count--;
+            }
+        }
+
+        public void Handle(EnterKeyboardCommand command)
+        {
+            _consoleText.Out.WriteLine();
+        }
+    }
+}
---
+++ test/FkThat.SecretInput.Tests/ConsoleTextAdapterTests.cs
@@ -0,0 +1,80 @@
+﻿using FkThat.Console;
+
+namespace FkThat.SecretInput.Tests;
+
+public class ConsoleTextAdapterTests
+{
+    [Theory]
+    [InlineData('*')]
+    [InlineData('\0')]
+    public void Handle_CharKeyboardCommand_should_write_char(char maskChar)
+    {
+        StringWriter sw = new();
+        var consoleText = A.Fake<IConsoleText>();
+        A.CallTo(() => consoleText.Out).Returns(sw);
+
+        ConsoleTextAdapter sut = new(consoleText, maskChar);
+        sut.Handle(new CharKeyboardCommand('Z'));
+
+        if (maskChar != '\0')
+        {
+            sw.ToString().Should().Be($"{maskChar}");
+        }
+        else
+        {
+            sw.ToString().Should().BeEmpty();
+        }
+    }
+
+    [Theory]
+    [InlineData('*')]
+    [InlineData('\0')]
+    public void Handle_BackspaceKeyboardCommand_should_backspace_when_not_empty(char maskChar)
+    {
+        StringWriter sw = new();
+        var consoleText = A.Fake<IConsoleText>();
+        A.CallTo(() => consoleText.Out).Returns(sw);
+
+        ConsoleTextAdapter sut = new(consoleText, maskChar);
+        sut.Handle(new CharKeyboardCommand('F'));
+        sut.Handle(new BackspaceKeyboardCommand());
+
+        if (maskChar != '\0')
+        {
+            sw.ToString().Should().Be($"{maskChar}\b \b");
+        }
+        else
+        {
+            sw.ToString().Should().BeEmpty();
+        }
+    }
+
+    [Theory]
+    [InlineData('*')]
+    [InlineData('\0')]
+    public void Handle_BackspaceKeyboardCommand_should_donothing_when_empty(char maskChar)
+    {
+        StringWriter sw = new();
+        var consoleText = A.Fake<IConsoleText>();
+        A.CallTo(() => consoleText.Out).Returns(sw);
+
+        ConsoleTextAdapter sut = new(consoleText, maskChar);
+        sut.Handle(new BackspaceKeyboardCommand());
+
+        sw.ToString().Should().BeEmpty();
+    }
+
+
+    [Fact]
+    public void Haandle_EnterKeyboardCommand_should_write_new_line()
+    {
+        StringWriter sw = new();
+        var consoleText = A.Fake<IConsoleText>();
+        A.CallTo(() => consoleText.Out).Returns(sw);
+
+        ConsoleTextAdapter sut = new(consoleText, '*');
+        sut.Handle(new EnterKeyboardCommand());
+
+        sw.ToString().Should().Be($"{Environment.NewLine}");
+    }
+}
